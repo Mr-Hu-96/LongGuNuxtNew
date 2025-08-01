7fe6efd5d8b0f4f08e58bd000ce7d477a0b7fe4c
jethro <mrhu2017web@gmail.com>
2025-08-01 18:37:16 +0800

    perf： 添加tailwind

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+    </div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
